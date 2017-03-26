9a2bb8dab2cee3603e7d75512fe734f7de3d9a5e
Seung-hae KIM <aka.seunghaekim@gmail.com>
2017-03-26 21:48:14 +0900

    init

---
+++ build_todolist.php
@@ -0,0 +1,23 @@
+<?php
+    $get_config = file_get_contents('./config.json', 'r');
+    $config = json_decode($get_config);
+
+    // mapping the config.json to variable
+    $baseurl = $config->{'baseurl'};
+    $baseurl = $baseurl.'archives';
+
+    $archives = file_get_contents($baseurl, 'r');
+
+    $dom = new DOMDocument;
+    $dom -> loadHTML($archives);
+    $todo_lv1 = array();
+
+    foreach($dom -> getElementsByTagName('a') as $todo) {
+        $attr = $todo->getAttribute('href');
+        $checker = preg_match('/\/archives\/[0-9]{4}$/', $attr);
+        if ($checker > 0) {
+            $todo_lv1[] = $attr;
+        };
+    };
+
+ ?>
