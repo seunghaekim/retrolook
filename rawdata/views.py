addbfe1dd045e0a4ec5363d39ea3b25a83236a13
seunghaekim <aka.seunghaekim@gmail.com>
2017-11-19 00:42:30 +0900

    modified:   rawdata/views.py: CHANGE crawler loop to simpler

--- rawdata/views.py
+++ rawdata/views.py
@@ -26,39 +26,23 @@ def latestCrawled(request):
 
 def crawler():
     crawler = Crawler()
-    print('collect years start')
-    years = [year for year in crawler.collectYears()]
-    print('%s collect years done' % datetime.datetime.now())
-    print('%s collect months start' % datetime.datetime.now())
-    months = [month for year in list(map(lambda y: crawler.collectMonths(y), years)) for month in year]
-    print('%s collect months done' % datetime.datetime.now())
-    print('%s collect days start' % datetime.datetime.now())
-    days = [day for month in list(map(lambda m: crawler.collectDays(m), months)) for day in month]
-    print('%s collect days done' % datetime.datetime.now())
-    for day in days:
-        data = {
-            'document': crawler.collectContent(day),
-            'uri': day,
-            'createtime': datetime.datetime.now(),
-            'pubtime': time.strftime(
-                '%Y-%m-%d',
-                time.strptime(
-                    re.compile(r'[^\d]').sub('', day.split('/')[-1]),
-                    '%Y%m%d'
-                )
-            )
-        }
-        if Rawdata.objects.filter(uri=day).count() < 1:
-            rawdata = Rawdata(
-                document=str(crawler.collectContent(day)),
-                uri=day,
-                createtime=datetime.datetime.now(),
-                pubtime=time.strftime('%Y-%m-%d', time.strptime(
+    for year in crawler.collectYears():
+        print('%s %s' % (datetime.datetime.now(), year))
+        for month in crawler.collectMonths(year):
+            print('%s %s' % (datetime.datetime.now(), month))
+            for day in crawler.collectDays(month):
+                if Rawdata.objects.filter(uri=day).count() > 0:
+                    continue
+                rawdata = Rawdata(
+                    document=str(crawler.collectContent(day)),
+                    uri=day,
+                    createtime=datetime.datetime.now(),
+                    pubtime=time.strftime('%Y-%m-%d', time.strptime(
                         re.compile(r'[^\d]').sub('', day.split('/')[-1]), '%Y%m%d'
-                )),
-                origin='neolook'
-            )
-            rawdata.save()
-            print('%s %s' % (datetime.datetime.now(), rawdata.uri))
+                    )),
+                    origin='neolook'
+                )
+                rawdata.save()
+                print('%s %s' % (datetime.datetime.now(), rawdata.uri))
 
     return None
